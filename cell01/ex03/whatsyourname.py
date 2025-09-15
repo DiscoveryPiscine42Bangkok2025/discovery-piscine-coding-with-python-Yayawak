e5b1998c867f4254e66653320432c16106081b0f
Madara <elio.interesante@gmail.com>
2025-09-15 19:44:29 +0700

    .

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Ask user for first and last name
+first_name = input("Enter your first name:\n")
+last_name = input("Enter your last name:\n")
+
+# Print greeting
+print("Hello,", first_name, last_name + "!")
