1692f58dc620e7f75611f9b725d12fd66a2d48ab
Madara <elio.interesante@gmail.com>
2025-09-15 13:48:58 +0700

    added c01s

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Apisit"
+last_name = "Thaweboon"
+print(f"{first_name} {last_name}")
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Apisit"
+last_name = "Thaweboon"
+whole_name = f"{first_name} {last_name}"
+print(whole_name)
